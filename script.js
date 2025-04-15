629ad3049b2f536dbb09ef56407098d1bcc68b74
Alyssa Pivirotto <44910695+ampivirotto@users.noreply.github.com>
2025-04-15 15:01:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 d3.csv("./data/longest_speeches.csv").then(function(data) {
-  // Convert time strings to numbers
+  // Convert 'seconds' to numbers
   data.forEach(d => {
     d.TimeInt = +d.TimeInt;
   });
 
-  // Log it just to check
-  console.log("Loaded data:", data);
+  const timeline = d3.select("#timeline");
+  const maxTime = d3.max(data, d => d.TimeInt);
+  const scale = d3.scaleLinear().domain([0, maxTime]).range([0, 9000]);
 
-  // Build your timeline here
-  drawTimeline(data);
+  timeline
+    .selectAll(".bar")
+    .data(data)
+    .join("div")
+    .attr("class", d => "bar" + (d.Name === "Cory Booker" ? " booker" : ""))
+    .style("width", d => scale(d.TimeInt) + "px");
 });
+
+
 
 
 
